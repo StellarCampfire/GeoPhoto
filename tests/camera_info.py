6f602766c74194c5dbd7cf6b579d8b3a0f3f5e9e
Tundra <Ssirey30zero@gmail.com>
2024-03-21 16:38:37 +0400

    Cameras info script

---
+++ tests/camera_info.py
@@ -0,0 +1,5 @@
+from picamera2 import Picamera2
+
+print("_______________________________________________")
+print(Picamera2.global_camera_info())
+print("_______________________________________________")
