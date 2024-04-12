d03304c9ee27e95c9009d7d6f6bff7477f336e28
Tundra <Ssirey30zero@gmail.com>
2024-04-12 22:59:59 +0300

    fix

--- src/utils/photo_manager/photo_manager.py
+++ src/utils/photo_manager/photo_manager.py
@@ -46,13 +46,19 @@ class CameraContextManager:
         self.camera = Picamera2(self.camera_index)
         config = self.camera.create_still_configuration()
         self.camera.configure(config)
+        logging.debug(f'CameraContextManager {self.camera_index}: starting camera...')
         self.camera.start()
+        logging.debug(f'CameraContextManager {self.camera_index}: camera started.')
         return self.camera
 
     def __exit__(self, exc_type, exc_value, traceback):
+        logging.debug(f'CameraContextManager {self.camera_index}: stoping camera...')
         self.camera.stop()
+        logging.debug(f'CameraContextManager {self.camera_index}: camera stoped.')
         del self.camera
+        logging.debug(f'CameraContextManager {self.camera_index}: camera deleted.')
         gc.collect()
+        logging.debug(f'CameraContextManager {self.camera_index}: gc collected.')
 
 class PhotoManager(BasePhotoManager):
     def __init__(self, temp_storage="temp/photos"):
@@ -60,18 +66,26 @@ class PhotoManager(BasePhotoManager):
         self.camera_indexes = [0, 1]
 
     def take_photo_with_camera_and_free_mem(self, camera_index, photo_path):
+        logging.debug(f'take_photo_with_camera_and_free_mem {camera_index}: starting ctx ...')
         with CameraContextManager(camera_index) as camera:
+            logging.debug(f'take_photo_with_camera_and_free_mem {camera_index}: ctx started')
             camera.capture_file(photo_path)
+
+        logging.debug(f'take_photo_with_camera_and_free_mem {camera_index}: ctx closed.')
         gc.collect()
+        logging.debug(f'take_photo_with_camera_and_free_mem {camera_index}: gc collected.')
         return photo_path
 
     def take_photos(self, project, well):
         """Initiates photo capture on all cameras in separate threads."""
+        logging.debug("take_photos: Starting taking photo process ..... ")
         result = []
         for camera_index in self.camera_indexes:
             photo_path = os.path.join(self.temp_photo_path,
                                       self.generate_unique_photo_name(project, well, camera_index))
+            logging.debug(f'take_photos: start take_photo_camera_and_free_mem for camera {camera_index}')
             photo_result = self.take_photo_with_camera_and_free_mem(camera_index, photo_path)
+            logging.debug(f'take_photos: finish take_photo_camera_and_free_mem for camera {camera_index}')
             result.append(photo_result)
 
         return result
